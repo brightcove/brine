c4b121a95f555b59e33f375f77cdc9c38ce1242b
Matt Whipple <matt@mattwhipple.com>
2017-07-28 06:40:33 -0400

    Improved path traversal and any selector (Fix #37) (#52)

---
+++ features/selectors/any.feature
@@ -0,0 +1,50 @@
+Feature: Assertions can be done against any element of a structure
+
+  Background
+    Given brine is mixed
+
+  Scenario: Assorted positive and negative assertions.
+    Given a file named "features/any.feature" with:
+      """
+
+Feature: Allow selection of any structure element
+  Scenario: List in response body
+    When the response body is assigned:
+      \"\"\"
+      ["a", "b", "c"]
+      \"\"\"
+    Then the value of the response body does have any element that is equal to `a`
+    Then the value of the response body does not have any element that is equal to `d`
+
+  Scenario: Nested list in response body
+    When the response body is assigned:
+      \"\"\"
+      {"letters": ["a", "b", "c"]}
+      \"\"\"
+    Then the value of the response body child `letters` does have any element that is equal to `a`
+    Then the value of the response body child `letters` does not have any element that is equal to `d`
+
+  Scenario: Map matches entries
+    When the response body is assigned:
+      \"\"\"
+      {"a": 1, "b": 2}
+      \"\"\"
+    #Equality will match keys
+    Then the value of the response body does have any element that is equal to `a`
+    Then the value of the response body does not have any element that is equal to `d`
+
+  Scenario: Supports flattened list nesting
+    When the response body is assigned:
+      \"\"\"
+      [{"val":"foo"},{"val":"bar"}]
+      \"\"\"
+    Then the value of the response body child `*.val` does have any element that is equal to `foo`
+    Then the value of the response body child `*.val` does not have any element that is equal to `other`
+
+      """
+    When I run `cucumber --strict features/any.feature`
+    Then the output should contain:
+      """
+      4 passed
+      """
+    And it should pass
--- lib/brine/selector.rb
+++ lib/brine/selector.rb
@@ -1,5 +1,6 @@
 require 'brine/coercer'
 require 'rspec/expectations'
+require 'brine/path_finder'
 
 # Selectors here are small wrappers around RSpec
 # expectation behavior to encapsulate variations in
@@ -13,10 +14,20 @@ class Selector
     @message = negated ? :to_not : :to
   end
 
+  def filter_matcher(matcher)
+    matcher
+  end
+
   def assert_that(value)
     target, value = coercer.coerce(@target, value)
-    matcher = yield(value)
+    matcher = filter_matcher(yield(value))
     expect(target).send(@message, matcher)
+  end
+end
+
+class AnySelector < Selector
+  def filter_matcher(matcher)
+    include(matcher)
   end
 end
 
@@ -42,33 +53,32 @@ Then(/^the value of `([^`]*)` is( not)? (.*)$/) do |value, negated, assertion|
 end
 
 RESPONSE_ATTRIBUTES='(status|headers|body)'
-Then(/^the value of the response #{RESPONSE_ATTRIBUTES} is( not)? (.*)(?<!:)$/) do
-  |attribute, negated, assertion|
-  use_selector(Selector.new(dig_from_response(attribute), (!negated.nil?)))
-  step "it is #{assertion}"
-end
-
-Then(/^the value of the response #{RESPONSE_ATTRIBUTES} is( not)? (.*)(?<=:)$/) do
-  |attribute, negated, assertion, multi|
-  use_selector(Selector.new(dig_from_response(attribute), (!negated.nil?)))
-  step "it is #{assertion}", multi.to_json
-end
-
-Then(/^the value of the response #{RESPONSE_ATTRIBUTES} child `([^`]*)` is( not)? (.*)(?<!:)$/) do
+Then(/^the value of the response #{RESPONSE_ATTRIBUTES}(?: child `([^`]*)`)? is( not)? (.*)(?<!:)$/) do
   |attribute, path, negated, assertion|
   use_selector(Selector.new(dig_from_response(attribute, path), (!negated.nil?)))
   step "it is #{assertion}"
 end
 
-Then(/^the value of the response #{RESPONSE_ATTRIBUTES} child `([^`]*)` is( not)? (.*)(?<=:)$/) do
+Then(/^the value of the response #{RESPONSE_ATTRIBUTES}(?: child `([^`]*)`)? is( not)? (.*)(?<=:)$/) do
   |attribute, path, negated, assertion, multi|
-  use_selector(Selector.new(dig_from_response(attribute, path), (!negated.nil?)))
+  use_selector(Selector.new(dig_from_response(attribute), (!negated.nil?)))
   step "it is #{assertion}", multi.to_json
 end
 
+Then(/^the value of the response #{RESPONSE_ATTRIBUTES}(?: child `([^`]*)`)? does( not)? have any element that is (.*)(?<!:)$/) do
+  |attribute, path, negated, assertion|
+  use_selector(AnySelector.new(dig_from_response(attribute, path), (!negated.nil?)))
+  step "it is #{assertion}"
+end
+
+Then(/^the value of the response #{RESPONSE_ATTRIBUTES}(?: child `([^`]*)`)? does( not)? have any element that is (.*)(?<=:)$/) do
+  |attribute, path, negated, assertion, multi|
+  use_selector(AnySelector.new(dig_from_response(attribute), (!negated.nil?)))
+  step "it is #{assertion}", multi.to_json
+end
 
 def dig_from_response(attribute, path=nil)
   root = response.send(attribute.to_sym)
   return root if !path
-  root.dig(*(path.to_s.split('.').map{|it| Integer(it) rescue it}))
+  PathFinder.new(path).walk(root)
 end
