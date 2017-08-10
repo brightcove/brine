6194a7492455344f102b82a3403f5c5e27456ab0
Taylor Corrello <tcorrello@tcorrello-retina17.vidmark.local>
2017-08-10 13:22:16 -0400

    adding options and head request methods

--- lib/brine/step_definitions/request_construction.rb
+++ lib/brine/step_definitions/request_construction.rb
@@ -8,7 +8,7 @@ When(/^the request body is assigned:$/) do |input|
   set_request_body(input)
 end
 
-When(/^a (GET|POST|PATCH|PUT|DELETE) is sent to `([^`]*)`$/) do |method, url|
+When(/^a (GET|POST|PATCH|PUT|DELETE|HEAD|OPTIONS) is sent to `([^`]*)`$/) do |method, url|
   send_request(parse_method(method), URI.escape(url))
   bind('response', response)
   reset_request
