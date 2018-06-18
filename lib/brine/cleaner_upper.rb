f8a65cdae033cf2fbe062169fcf6f8663f243bed
Matt Whipple <mwhipple@brightcove.com>
2018-06-18 13:37:11 -0400

    Document some classes

--- lib/brine/cleaner_upper.rb
+++ lib/brine/cleaner_upper.rb
@@ -52,29 +52,49 @@ end
 #
 # Exposes methods to keep a stack of DeleteCommands corresponding to each
 # created resource which are then popped and invoked to perform the cleanup.
+#
+# The LIFO behavior is adopted as it is more likely to preserve integrity,
+# such as removing children added to parents or similar dependencies.
 module CleanerUpper
 
-  # HTTP client object used to issue DELETE calls
-  # must support #delete(path)
-  # to be injected by calling code
+  ##
+  # Set the Faraday HTTP client object used to issue DELETE calls.
+  #
+  # The client provided will be subsequently used to create DeleteCommands.
+  # This can be called multiple times where each DeleteCommand will use the
+  # most recently set value. In most use cases this will also be the client
+  # used to issue the creation requests and could therefore be passed to this
+  # method prior to use.
+  #
+  # @param client - The client to use to DELETE subsequently tracked resources.
   def set_cleaning_client(client)
     @client = client
   end
 
-  # Record resource to be cleaned
+  ##
+  # Record resource to be later cleaned (pushes a DeleteCommand).
+  #
+  # @param path - The path for the created resource, will be issued a DELETE.
   def track_created_resource(path)
     created_resources << DeleteCommand.new(@client, path)
   end
 
-  # Clean recorded resources
-  # Expected to be called after test run
+  ##
+  # Clean recorded resources (normally after a test run).
   def cleanup_created_resources
     created_resources.reverse.each{|it| it.cleanup}
   end
 
   private
 
-  # Lazily initialized array of resources to remove
+  ##
+  # The array which serves as the stack of DeleteCommands.
+  #
+  # Works as a "module provided property" which is a name I
+  # may have just made up.
+  #
+  # TODO: Find proper term for module provided property
+  # TODO: The name of this property seems sloppy as it contains commands.
   def created_resources
     @created_resources ||= []
   end
