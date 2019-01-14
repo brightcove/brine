b3a459a16a364628a9fa56acb5f19943cc2b156e
Gaurav Saxena <gsaxena@brightcove.com>
2019-01-14 16:11:14 -0500

    step to support HTTP Basic Auth

--- lib/brine/step_definitions/request_construction.rb
+++ lib/brine/step_definitions/request_construction.rb
@@ -1,4 +1,5 @@
 # request_construction.rb - Build and send requests
+require 'base64'
 
 When(/^the request body is assigned:$/) do |input|
   set_request_body(input)
@@ -10,6 +11,10 @@ end
 
 When(/^the request header `([^`]*)` is assigned `([^`]*)`$/) do |header, value|
   add_header(header, value)
+end
+
+When(/^the request auth basic header `([^`]*)` is assigned `([^`]*)`$/) do |header, value|
+  add_header(header, Base64.encode64(value))
 end
 
 When(/^an? (GET|POST|PATCH|PUT|DELETE|HEAD|OPTIONS) is sent to `([^`]*)`$/) do |method, url|
