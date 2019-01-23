91797caf614f6fbfcd00470945afdab8e27e5e4e
Matt Whipple <matt@mattwhipple.com>
2019-01-23 13:56:19 -0500

    Restructure for monorepo (Closes #75) (#150)

--- features/step_definitions/test_steps.rb
+++
@@ -1,5 +0,0 @@
-# the output should contain: is useful for seeing failures
-# TODO: Swap this out with standard aruba step
-Then(/^it should pass$/) do
-  expect(last_command_started).to be_successfully_executed
-end
