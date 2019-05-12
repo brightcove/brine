e44ce1dc4565a6400046167764b5b735d74e5c58
Lewis Cowles <lewiscowles@me.com>
2019-05-12 23:08:10 +0100

    Added Basic Auth

---
+++ features/request_construction/basic_auth.feature
@@ -0,0 +1,13 @@
+Feature: Adding Basic Auth
+
+  Scenario: A new header with expected value is added to request.
+    Given expected request headers:
+      """
+      {"Authorization":"Basic dXNlcjpwYXNz"}
+      """
+    And expected GET sent to `/query` with 
+
+    When the request credentials are set for basic auth user `user` and password `pass`
+    And a GET is sent to `/query`
+
+    Then expected calls are verified
--- ruby/lib/brine/step_definitions/request_construction.rb
+++ ruby/lib/brine/step_definitions/request_construction.rb
@@ -12,6 +12,11 @@ When(/^the request header `([^`]*)` is assigned `([^`]*)`$/) do |header, value|
   add_header(header, value)
 end
 
+When(/^the request credentials are set for basic auth user `([^`]*)` and password `([^`]*)`$/) do |user, password|
+  base64_combined = Base64.strict_encode64("#{user}:#{password}")
+  add_header('Authorization', "Basic #{base64_combined}")
+end
+
 When(/^an? (GET|POST|PATCH|PUT|DELETE|HEAD|OPTIONS) is sent to `([^`]*)`$/) do |method, url|
   send_request(parse_method(method), URI.escape(url))
   bind('response', response)
